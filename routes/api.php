a0b52ed1eb149b319d9094820cbcf1b3397ef5b1
ibrahimi alshami <ibrahim77idalshami@gmail.com>
2025-09-01 09:10:43 +0300

    feat: عرض الاقسام

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -16,8 +17,10 @@ Route::get("/categories", function () {
     ]);
 });
 
-Route::post("/categories", function (Request $request) {
-  $user= User::create($request->all());
+// Route::post("/categories", function (Request $request) {
+//   $user= User::create($request->all());
 
-  return response()->json($user, 201);
-});
+//   return response()->json($user, 201);
+// });
+
+Route::get("/categories", [CategoryController::class,'index']);
